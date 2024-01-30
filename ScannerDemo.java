acbd88b7ae84558f164e3ac05a2022c682a27962
PatilDarsh <darshpatil072@gmail.com>
2024-01-30 20:33:58 +0530

    [ADDED]:Darshan java code for Scanner

---
+++ ScannerDemo.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class ScannerDemo
+{
+
+	public static void main(String args[])
+	{
+		float a,b,c;
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter 2 Number");
+		
+		a=sc.nextFloat();
+		b=sc.nextFloat();
+		c=a+b;
+		System.out.println("Addition="+c);
+	}
+}
+		
